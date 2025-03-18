1d98096fa2adc587aa72408f9aa8477a20032e06
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2025-03-18 15:42:16 +0300

    ipdateusername

--- ApiForTravel/Models/UserModel.cs
+++ ApiForTravel/Models/UserModel.cs
@@ -11,6 +11,7 @@ namespace ApiForTravel.Models
 
         public string? Email { get; set; }
         public string? Password { get; set; }
+        public string? Username { get; set; }
         public string RefreshToken { get; set; }
         public DateTime RefreshTokenExpiryTime { get; set; }
     }
